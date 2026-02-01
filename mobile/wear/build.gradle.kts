d3650d0996544a222c64f38e1b71ad615268fe0c
rylieh31 <121984095+rylieh31@users.noreply.github.com>
2026-01-31 22:32:39 -0500

    Watch App UI made

--- mobile/wear/build.gradle.kts
+++ mobile/wear/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation(libs.androidx.wear.tooling.preview)
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.core.splashscreen)
+    implementation(libs.androidx.compose.material3)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
